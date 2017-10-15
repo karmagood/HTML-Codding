e389488ff381129c12db46ddbe9715afdfdb1383
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2017-10-16 00:39:15 +0300

    added mustached facebook

---
+++ Sergii_Dubovyk/hw4/mustached_facebook/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, send_from_directory
+import pystache
+import codecs
+import simplejson as json
+
+
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/')
+def hello_world():
+    f = codecs.open('templates/index.html', encoding='utf-8')
+    view = f.read()
+
+    data_file = codecs.open('data.json', encoding='utf-8')
+    data = json.load(data_file)
+
+    rendered = pystache.render(view, data)
+    print(rendered)
+    return rendered
+
+
+@app.route('/css/<path:path>')
+def send_js(path):
+    return send_from_directory('static/css', path)
+
+
+@app.route('/img/<path:path>')
+def send_img(path):
+    return send_from_directory('static/img', path)
+
+
+if __name__ == '__main__':
+    app.run()
