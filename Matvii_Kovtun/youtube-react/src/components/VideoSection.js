339da67eb2b43183cf3aa68d88445e3e3e7652ed
kovtun_m <kovtun_m@ucu.edu.ua>
2017-11-04 14:36:14 +0200

    Reworked structure of payload, added more js files. Added side menu.

--- Matvii_Kovtun/youtube-react/src/components/VideoSection.js
+++ Matvii_Kovtun/youtube-react/src/components/VideoSection.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import Video from './Video';
+import '../style/VideoSection.css';
+import videos from '../data/videos';
 
-import '../style/VideoSection.css'
 
-const videos = [
-    {name: "My Videos"},
-    {}
-]
-
-const VideoSection = () => (
+const VideoSection = ({sectionTitle}) => (
     <section class="video-section">
-        <h2 class="video-section__title">Lorem ipsum dolor.</h2>
+        <h2 class="video-section__title">{sectionTitle}</h2>
         <div class="video-section__feed">
-            {videos.map( (props) => <Video {...props} />)}
+            {videos.map(({title, videoViews, img, time, channel}) =>
+                <Video {...{title, videoViews, img, time, channel}} />)}
             <Video/>
-            <div class="video-section__video">lorem5</div>
-            <div class="video-section__video">lorem5</div>
-            <div class="video-section__video">lorem5</div>
-            <div class="video-section__video">lorem5</div>
         </div>
     </section>
 
-)
+);
 
 
 export default VideoSection;
