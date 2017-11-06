b68e631136e703c22f85f3a092531e11897581bd
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2017-11-06 09:49:58 +0200

    fixed min/max array element finding

--- Sergii_Dubovyk/jsalgofun_site/routes/algorithm.js
+++ Sergii_Dubovyk/jsalgofun_site/routes/algorithm.js
@@ -7,6 +7,14 @@ var bubblesort = require('../algorithms/sort/bubblesort');
 
 const sort_algo = require('../algorithms/sort/algo_sort');
 
+const getArrayMaxVal = (x) => {
+    return Math.max.apply(null, x);
+}
+
+const getArrayMinVal = (x) => {
+    return Math.min.apply(null, x);
+}
+
 router.get('/:algorithmName', function (req, res, next) {
     var problemData = global_data['sort_' + req.params.algorithmName];
     if (problemData === null){
@@ -36,12 +44,15 @@ router.get('/:algorithmName', function (req, res, next) {
     if (req.body.sort_data){
         data_array = true;
         nums_array = req.body.sort_data.replace(/[^\d,-]/g, '').split(',');
+        for(let i = 0; i < nums_array.length; i++) nums_array[i] = parseInt(nums_array[i]);
         if(req.params.algorithmName === 'bubble'){
             comparisons = sort_algo.bubble_sort(nums_array);
         } else if(req.params.algorithmName === 'insert'){
             comparisons = sort_algo.insertion_sort(nums_array);
         } else if(req.params.algorithmName === 'count'){
-            comparisons = sort_algo.counting_sort(nums_array, 0, nums_array.length);
+            console.log(Math.min(nums_array), Math.max(nums_array));
+            sort_algo.counting_sort(nums_array, getArrayMinVal(nums_array), getArrayMaxVal(nums_array));
+            comparisons = 'a lot of'
         } else if(req.params.algorithmName === 'merge'){
             comparisons = sort_algo.merge_sort(nums_array);
         } else if(req.params.algorithmName === 'select'){
