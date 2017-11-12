4fc2e09acaf7107e2818231f0cd50f411904c305
Bohdan Petryshak <bohdanpv@gmail.com>
2017-11-12 22:55:54 +0200

    dev process

---
+++ Bohdan_Petryshak/js-lab/javascript-add.js
@@ -0,0 +1,21 @@
+//1 --------------------------------------
+let h1 = document.createElement('h1');
+let text = document.createTextNode("heLLo!");
+
+// document.body.appendChild(h1);
+//
+// h1.appendChild(text);
+//
+// let button = document.createElement('button');
+// let buttonText = document.createTextNode('click me');
+// button.appendChild(buttonText);
+
+// button.onclick = (ev) => {
+//     let newText = document.createTextNode('world');
+//     h1.appendChild(newText)
+// };
+
+// document.body.appendChild(button);
+// 2------------------------------------------------
+
+// const button = document.getElementsByTagName('h1');
