47b92a20c2dbaa4794c53c66aba223c7bf260415
KushnirDmytro <kushnir.dmytro.93@gmail.com>
2017-11-19 15:52:48 +0200

    quicksort Autotest added to Utils

--- Dmytro_Kushnir/js_algos/quick_sort.js
+++ Dmytro_Kushnir/js_algos/quick_sort.js
@@ -1,9 +1,13 @@
 
-const {swap, getRandomArray, getRandomNumber} = require("./utility.js");
+const {swap, getRandomArray, getRandomNumber, isArraySorted, checkArraySorting} = require("./utility.js");
 
 
+const quickSort = (test_arr) => {
+    return quickSortAlgo(test_arr, 0, test_arr.length);
+};
 
-const quickSort = (arr, l , r) =>{
+
+const quickSortAlgo = (arr, l , r) =>{
     /*
     * sorts using 1 pivot method of sorting in same memory.
      *  */
@@ -26,12 +30,16 @@ const quickSort = (arr, l , r) =>{
     }
 
     if (arr[l] > arr[newPivotPosition] ) swap(arr, l, newPivotPosition); // place pivot on its new place
-    quickSort(arr, l , newPivotPosition);
-    quickSort(arr, newPivotPosition+1, r);
+    quickSortAlgo(arr, l , newPivotPosition);
+    quickSortAlgo(arr, newPivotPosition+1, r);
+    return arr;
 };
 
 let test_arr = getRandomArray(5);
 
+
 console.log(test_arr);
-quickSort(test_arr, 0, test_arr.length);
+quickSortAlgo(test_arr, 0, test_arr.length);
 console.log(test_arr); // array is mutated
+
+checkArraySorting(iterations = 1000, testSizeFrom = 3, testSizeTo = 300, sortingFunction = quickSort);
--- Dmytro_Kushnir/js_algos/utility.js
+++ Dmytro_Kushnir/js_algos/utility.js
@@ -1,10 +1,40 @@
 const swap = (arr, i, j) => {
-    var tmp = arr[i];
+    let tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
     return arr;
 };
 
+
+const checkArraySorting = (iterations, testSizeFrom, testSizeTo, sortingFunction ) => {
+    let i = 0, isSorted = true;
+    let original_array_holder;
+    while (i++ < iterations && isSorted){
+        test_arr = getRandomArray(getRandomNumber(testSizeFrom,testSizeTo));
+        original_array_holder = test_arr.slice();
+        test_arr = sortingFunction(test_arr); //quickSort(test_arr, 0, test_arr.length);
+        isSorted = isArraySorted(test_arr);
+        // console.log(i)
+    }
+    if (!isSorted){
+        console.log("sorting FAIL");
+        console.log(original_array_holder);
+        console.log(test_arr);
+    }
+    else
+        console.log("sorting OK");
+    console.log( i-1 + " iterations done. " + "Test_size from " + testSizeFrom + " to " + testSizeTo);
+};
+
+const isArraySorted = (arr) => {
+    let isSorted = true;
+
+    for (let i = 0; i < arr.length-1 && isSorted; i++){
+        if (arr[i] > arr[i+1])
+            isSorted = false;
+    }
+    return isSorted;
+};
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -37,5 +67,7 @@ const numberOfSimpleDivisors = (arg) => {
 module.exports = {
     getRandomNumber,
     getRandomArray,
-    swap
+    swap,
+    isArraySorted,
+    numberOfSimpleDivisors, checkArraySorting
 };
