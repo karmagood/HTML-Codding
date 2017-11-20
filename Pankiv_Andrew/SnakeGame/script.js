eeefdc5b6cfc43081431b06bc15f27106a2f30bc
PankivAndrew <pankiv_a@ucu.edu.ua>
2017-11-20 14:24:54 +0200

    Bug fixes

--- Pankiv_Andrew/SnakeGame/script.js
+++ Pankiv_Andrew/SnakeGame/script.js
@@ -6,7 +6,6 @@ let Width = 30;
 let interval = 150;
 let growth = 2;
 
-
 //game variables
 let length = 0;
 let TailX = [SnakeX];
@@ -16,22 +15,33 @@ let FruitY;
 let Running = false;
 let GameOver = false;
 let WallKill = false;
+let ChangePortals = false;
 let direction = 2;// up = 8 , down = 2, right = 6, left = 4
-let inter;
+let Inter;
+let Timer;
+let Seconds = 10;
 let Score = 0;
 let BestScore = 0;
 let Beginning = true;
+let PortalPositions = [[[13, 0], [14, 0], [15, 0], [16, 0]],
+    [[13, 29], [14, 29], [15, 29], [16, 29]],
+    [[0, 13], [0, 14], [0, 15], [0, 16]],
+    [[29, 13], [29, 14], [29, 15], [29, 16]]];
+
+
 
 function Run() {
     Init();
-    inter = setInterval(GameLoop, interval);
+    Inter = setInterval(GameLoop, interval);
+    Timer = setInterval(PortalTime,1000);
 
 }
 
 function Reset() {
     DeleteSnake();
-    clearInterval(inter);
+    clearInterval(Inter);
     RebuildMap();
+    ReCreatePortals();
     Score = 0;
     document.getElementById("score").innerHTML = '' + Score;
     length = 0;
@@ -45,7 +55,7 @@ function Reset() {
     WallKill = false;
     Running = false;
     CreateSnake();
-    inter = setInterval(GameLoop, interval);
+    Inter = setInterval(GameLoop, interval);
 
 }
 
@@ -53,6 +63,17 @@ function Init() {
     CreateMap();
     CreateSnake();
     CreateFruit();
+    ReCreatePortals();
+}
+
+/**
+ * @return {boolean}
+ */
+function InPortalPositions(x, y) {
+    if (x !== undefined && y !== undefined) {
+        return GetType(x, y) === "portal";
+    }
+
 }
 
 function CreateMap() {
@@ -60,7 +81,7 @@ function CreateMap() {
     for (let y = 0; y < Height; y++) {
         document.write("<tr>");
         for (let x = 0; x < Width; x++) {
-            if (x === 0 || x === Width - 1 || y === 0 || y === Height - 1) {
+            if ((x === 0 || x === Width - 1 || y === 0 || y === Height - 1)) {
                 document.write("<td class='wall' id='" + x + "-" + y + "'></td>");
             } else {
                 document.write("<td class='blank' id='" + x + "-" + y + "'></td>");
@@ -83,6 +104,42 @@ function CreateSnake() {
     Set(SnakeX, SnakeY, "snake");
 }
 
+function ReCreatePortals() {
+    for (let line = 0; line < PortalPositions.length; line++) {
+        if (ChangePortals) {
+            for (let position = 0; position < PortalPositions[line].length; position++) {
+                if (line < 2) {
+                    Set(PortalPositions[line][position][0], PortalPositions[line][position][1], "portal");
+                }else{
+                    Set(PortalPositions[line][position][0], PortalPositions[line][position][1], "wall");
+                }
+            }
+        }
+        else if (!ChangePortals) {
+            for (let position = 0; position < PortalPositions[line].length; position++) {
+                if (line > 1) {
+                    Set(PortalPositions[line][position][0], PortalPositions[line][position][1], "portal");
+                }
+                else{
+                    Set(PortalPositions[line][position][0], PortalPositions[line][position][1], "wall");
+                }
+            }
+        }
+    }
+    ChangePortals = !ChangePortals;
+}
+
+function PortalTime() {
+    if(Seconds < 0){
+        Seconds = 10;
+    }
+    document.getElementById("timer").innerHTML = "Seconds to switch portals: " + Seconds;
+    if(Seconds === 0){
+        ReCreatePortals();
+    }
+    Seconds--;
+}
+
 function DeleteSnake() {
     for (let i = 0; i < length; i++) {
         Set(TailX[i], TailY[i], "blank");
@@ -93,6 +150,7 @@ function DeleteSnake() {
 function Get(x, y) {
     return document.getElementById(x + "-" + y);
 }
+
 
 function Set(x, y, value) {
     if (x !== undefined && y !== undefined) {
@@ -125,6 +183,8 @@ window.addEventListener("keydown", function key() {
     if (!Running) {
         Running = true;
     }
+    console.log(key);
+    sleep(20);
     switch (key) {
         case 37:
             if (direction !== 6 || Beginning) {
@@ -132,18 +192,18 @@ window.addEventListener("keydown", function key() {
             }
             break;
         case 38:
-            if (direction !== 2|| Beginning){
+            if (direction !== 2 || Beginning) {
                 direction = 8;
             }
             break;
         case 39:
-            if(direction !== 4|| Beginning) {
+            if (direction !== 4 || Beginning) {
                 direction = 6;
             }
             break;
 
         case 40:
-            if (direction !== 8|| Beginning){
+            if (direction !== 8 || Beginning) {
                 direction = 2;
             }
             break;
@@ -161,14 +221,27 @@ function GameLoop() {
         Update();
     }
     else if (GameOver) {
-        clearInterval(inter);
+        clearInterval(Inter);
+    }
+}
+
+function sleep(milliseconds) {
+    let start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
     }
 }
 
 function Update() {
     Set(FruitX, FruitY, "fruit");
     UpdateTail();
-    Set(TailX[length], TailY[length], "blank");
+    if (!InPortalPositions(TailX[length], TailY[length])) {
+        Set(TailX[length], TailY[length], "blank");
+    }
+
+
     switch (direction) {
         case 4:
             SnakeX--;
@@ -183,37 +256,56 @@ function Update() {
             SnakeY++;
             break;
     }
+
     if (GetType(SnakeX, SnakeY) === "wall") {
+        sleep(100);
         WallKill = true;
         GameOver = true;
+        document.getElementById("last-score").innerHTML = "Last score : " + Score;
+        Reset();
+    }
+    if (!InPortalPositions(SnakeX, SnakeY) && !WallKill) {
+        Set(SnakeX, SnakeY, "snake");
+    }
+    if (InPortalPositions(SnakeX, SnakeY)) {
+        if (SnakeY === 0) {
+            SnakeY = 29;
+        } else if (SnakeY === 29) {
+            SnakeY = 0;
+        }
+        if (SnakeX === 0) {
+            SnakeX = 29;
+        } else if (SnakeX === 29) {
+            SnakeX = 0;
+        }
     }
 
-    Set(SnakeX, SnakeY, "snake");
     if (SnakeX === FruitX && SnakeY === FruitY) {
         CreateFruit();
         Beginning = false;
         length += growth;
-        if (interval >= 30){
+        if (interval >= 30) {
             interval -= 10;
-            clearInterval(inter);
-            inter = setInterval(GameLoop, interval);
+            clearInterval(Inter);
+            Inter = setInterval(GameLoop, interval);
         }
-        console.log(interval);
         Score++;
         document.getElementById("score").innerHTML = '' + Score;
     }
+
     for (let i = 0; i < length; i++) {
         if (SnakeX === TailX[i] && SnakeY === TailY[i]) {
             GameOver = true;
+            Reset();
+            document.getElementById("last-score").innerHTML = "Last score : " + Score;
             break;
         }
     }
-    if(Score > BestScore){
+
+    if (Score > BestScore) {
         BestScore = Score;
         document.getElementById("best-score").innerHTML = 'Best score : ' + BestScore;
     }
-
-
 }
 
 function UpdateTail() {
