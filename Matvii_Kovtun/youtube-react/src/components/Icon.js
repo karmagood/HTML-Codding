04e66800583fc0d5e3adb60fa835c80c68b25e6a
kovtun_m <kovtun_m@ucu.edu.ua>
2017-11-21 15:58:35 +0200

    Customized react youtube.

--- Matvii_Kovtun/youtube-react/src/components/Icon.js
+++ Matvii_Kovtun/youtube-react/src/components/Icon.js
@@ -3,9 +3,10 @@ import "../style/Icon.css"
 
 
 const Icon = ({icon, svg}) => (
+
     <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet"
          focusable="false"
-         class="icon">
+         class={"icon icon_" + icon}>
         <g>
             <path d={svg}></path>
         </g>
