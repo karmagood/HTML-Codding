941cd81c9cafbf80af516f85ada8d3e9e2b8a5bf
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2017-11-24 16:27:40 +0200

    Try new project structure.

--- Yurii_Laba/DevelopmentHelper/src/App.js
+++ Yurii_Laba/DevelopmentHelper/src/App.js
@@ -1,10 +1,19 @@
-import handlerbar from "handlebars"
+import handlerbar from "handlebars/dist/handlebars"
 
-const submitHandler = (event) => {
-    event.preventDefault();
+const renderBlock = (block, data) => {
+    let templateSrc = `blocks/${block}/${block}.handlebars`;
+    localStorage.setItem("block", block);
+    localStorage.setItem("data", data);
 
-    let templateSrc = "templates/" + event.target.templateSrc.value + ".handlebars";
-    let dataSrc = "data/" + event.target.dataSrc.value + ".json";
+    let dataSrc = "";
+
+    if (data.trim()) {
+        dataSrc = `data/${data}.json`;
+    } else {
+        dataSrc = `blocks/${block}/${block}.json`
+    }
+
+    let styleSrc = `blocks/${block}/${block}.css`;
 
     Promise.all([
         fetch(dataSrc)
@@ -18,26 +27,59 @@ const submitHandler = (event) => {
             })
             .then((text) => {
                 return handlerbar.compile(text);
+            }),
+        fetch(styleSrc)
+            .then((response) => {
+                return response.text();
             })
+
     ])
         .then((result) => {
             const data = result[0];
             const template = result[1];
-            return template(data)
+            const styles = result [2];
+
+            return [template(data), styles]
 
         })
-        .then((html) => {
+        .then(([html, styles]) => {
+            let styleSheet = document.createElement ("style");
+            styleSheet.innerText = styles;
+            document.head.appendChild(styleSheet);
+
             document.getElementById("payload").innerHTML = html;
         })
+}
 
+const setInitData = () => {
+    const block = localStorage.getItem("block");
+    const data = localStorage.getItem("data");
+
+    const form = document
+        .querySelector(".developer-helper");
+
+    form.templateSrc.value = block;
+    form.dataSrc.value = data;
+
+    if (block) {
+        renderBlock(block, data);
+    }
+};
+
+setInitData();
+
+
+const submitHandler = (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    const block = form.templateSrc.value;
+    const data = form.dataSrc.value;
+
+    renderBlock(block, data)
 
 };
 
 document
     .querySelector(".developer-helper")
     .addEventListener("submit", submitHandler);
-
-
-
-
-
