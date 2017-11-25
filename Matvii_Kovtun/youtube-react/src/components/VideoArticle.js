1246d0feee4ce945517d99050ef025ad8a51d235
kovtun_m <kovtun_m@ucu.edu.ua>
2017-11-25 23:51:17 +0200

    Developing second page

--- Matvii_Kovtun/youtube-react/src/components/VideoArticle.js
+++ Matvii_Kovtun/youtube-react/src/components/VideoArticle.js
@@ -1,24 +1,28 @@
 import React, {Component} from 'react';
 import '../style/VideoArticle.less';
 import VideoArticleInfo from './VideoArticleInfo';
-import VideoArticleDescription from './VideoArticleDescription';
-
+import VideoArticleDescription from './VideoDescription';
+import NextVideos from './NextVideos';
+import VideoComments from './VideoComments';
 
 class VideoArticle extends Component {
+
     render() {
+        //TODO: rename video-article
+        //TODO: try removing <div>...</div>
         return (
             <div>
                 <div style={{background: "black", width: "100%", height: "400px"}}></div>
                 <article className="video-article">
+                    <NextVideos/>
                     <h1 className="video-article__title">Summer Special Amazing Mix 2017 - Best Of Deep House Sessions
                         Music 2017 Chill Out Mix by Drop G</h1>
                     <VideoArticleInfo/>
                     <VideoArticleDescription/>
+                    <VideoComments/>
                 </article>
-                <div className="comments">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, rerum!</div>
-                <div className="next-videos" style={{position:"absolute", background: "black", left:"70%", width: "100%", height: "400px"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur earum
-                    fuga molestiae, nemo nostrum similique!
-                </div>
+
+
             </div>
         )
     }
--- Matvii_Kovtun/youtube-react/src/components/VideoArticleInfo.js
+++ Matvii_Kovtun/youtube-react/src/components/VideoArticleInfo.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import "../style/VideoArticle.less";
 
 
+
 class VideoArticleInfo extends Component {
     render() {
         return (
