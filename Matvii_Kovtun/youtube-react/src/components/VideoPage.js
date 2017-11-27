fc4d1d1e12857e44af47121417904428cc07fa9c
matvii.kovtun <kovtun_m@ucu.edu.ua>
2017-11-27 15:42:00 +0200

    Added fast modifier to style change in order to debug containers borders.

--- Matvii_Kovtun/youtube-react/src/components/VideoPage.js
+++ Matvii_Kovtun/youtube-react/src/components/VideoPage.js
@@ -9,16 +9,23 @@ class VideoArticle extends Component {
 
     render() {
         return (
-            <section className="video-page">
+            <section className="video-page video-page_highlight">
                 <div className="video-page__video"></div>
                 <article className="video-page__details">
                     <NextVideos/>
                     <h1 className="video-page__title">Summer Special Amazing Mix 2017 - Best Of Deep House Sessions
                         Music 2017 Chill Out Mix by Drop G
                     </h1>
-                    <VideoInfo/>
-                    <VideoArticleDescription/>
-                    <VideoComments/>
+                    <div className="video-page__info">
+                        <VideoInfo/>
+                    </div>
+                    <div className="video-page__article-description">
+                        <VideoArticleDescription/>
+                    </div>
+
+                    <div className="video-page__comments">
+                        <VideoComments/>
+                    </div>
                 </article>
             </section>
         )
