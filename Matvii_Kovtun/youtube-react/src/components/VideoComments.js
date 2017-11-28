1540e140b1806f89d318b0c82ba1f452069b8b66
matvii.kovtun <kovtun_m@ucu.edu.ua>
2017-11-29 00:06:33 +0200

    Added Comment component

--- Matvii_Kovtun/youtube-react/src/components/VideoComments.js
+++ Matvii_Kovtun/youtube-react/src/components/VideoComments.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 import "../style/VideoComments.less";
 import VideoAddComment from './VideoAddComment';
-
+import VideoComment from './VideoComment';
 
 class VideoComments extends Component {
     render() {
-        return [<VideoAddComment/>,
-            <div className="video-comments">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure,rerum!
-            </div>];
+        return [
+            <VideoAddComment/>,
+            <VideoComment/>];
     }
 }
 
