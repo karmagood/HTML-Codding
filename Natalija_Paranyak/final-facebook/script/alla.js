ab6378a8c49eb37ef67c13e8c62e0e06e196f005
paranyak <paranyak@ucu.edu.ua>
2017-11-29 14:38:51 +0200

    changed css files, added js file

---
+++ Natalija_Paranyak/final-facebook/script/alla.js
@@ -0,0 +1,3 @@
+// let s_height = document.getElementsByTagName('textarea').scrollHeight;
+// document.getElementById('textarea').setAttribute('style','height:'+s_height+'px');
+console.log("hello");
