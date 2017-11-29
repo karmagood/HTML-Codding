0b863e3f4c3062fb6c702984aa59540b701855c9
matvii.kovtun <kovtun_m@ucu.edu.ua>
2017-11-29 17:35:34 +0200

    Server now can work with html/mustache pages which use scripts written in js.

--- Natalija_Paranyak/final-facebook/script/alla.js
+++
@@ -1,3 +0,0 @@
-// let s_height = document.getElementsByTagName('textarea').scrollHeight;
-// document.getElementById('textarea').setAttribute('style','height:'+s_height+'px');
-console.log("hello");
---
+++ Natalija_Paranyak/final-facebook/scripts/alla.js
@@ -0,0 +1,3 @@
+let s_height = document.getElementsByTagName('textarea')[0].scrollHeight;
+document.getElementsByTagName('textarea')[0].setAttribute('style', 'height:' + s_height + 'px');
+console.log("hello ya pisya");
