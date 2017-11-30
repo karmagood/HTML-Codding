ec4833e90396810943cd3a8e084009c68810368d
paranyak <paranyak@ucu.edu.ua>
2017-11-30 14:52:21 +0200

    added textarea auto resized

--- Natalija_Paranyak/final-facebook/scripts/alla.js
+++ Natalija_Paranyak/final-facebook/scripts/alla.js
@@ -1,3 +1,10 @@
-let s_height = document.getElementsByTagName('textarea')[0].scrollHeight;
-document.getElementsByTagName('textarea')[0].setAttribute('style', 'height:' + s_height + 'px');
-console.log("hello ya pisya");
+const tx = document.getElementsByClassName('main-container__feeds__new__on-mind__input');
+for (let i = 0; i < tx.length; i++) {
+    tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+    tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+}
